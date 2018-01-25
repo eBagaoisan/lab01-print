4c419f20e1901232ba426393f61f3e8291b3dc1b
ebagaoisan <ebagaoisan1@mail.csuchico.edu>
2018-01-25 09:40:35 -0800

    First commit

---
+++ print.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Please enter a number: ";
+    cin>>num;
+    for (int i = 0; i<num; i++){
+        cout<<"hello"<<endl;
+    }
+
+    return 0;
+}
